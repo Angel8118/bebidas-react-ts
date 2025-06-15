11817f301771f8628df29ff3c85779939fbde110
Angel8118 <angel8118campos@gmail.com>
2025-06-15 12:29:35 -0600

    create Header as Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Header() {
+  return (
+    <header className="bg-slate-800" >
+        <div className="container mx-auto px-5 py16" >
+            <div className="flex justify-between items-center">
+                <div>
+                    <img src="/logo.svg" alt="logotipo" className="w-32" />
+                </div>
+
+                <nav>
+
+                </nav>
+
+            </div>
+        </div>
+
+    </header>
+  )
+}
